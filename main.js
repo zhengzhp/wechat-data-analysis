b1a4f41d854860160fe9e22624de1e900dc2ed92
zhengzhp <15075056375@163.com>
2019-04-29 10:34:22 +0800

    init electron

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 1200, height: 800 })
 
   // and load the index.html of the app.
   //判断是否是开发模式
